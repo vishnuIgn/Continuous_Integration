d7a46d91ba1611bf3f70d446cda070f8f5c4af47
vishnu <vishnu@ignitarium.com>
2021-02-11 19:58:17 +0530

    mm

--- calculator.py
+++ calculator.py
@@ -10,6 +10,3 @@ def add(first_term, second_term):
 def subtract(first_term, second_term):
     return first_term - second_term
 
-
-def multiply(first_term, second_term):
-    return first_term * second_term

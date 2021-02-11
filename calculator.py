403dba5352ec2fe9f233aeb3955735ef23064616
vishnu <vishnu@ignitarium.com>
2021-02-11 19:53:16 +0530

    line added

--- calculator.py
+++ calculator.py
@@ -2,6 +2,7 @@
 Calculator library containing basic math operations.
 """
 
+
 def add(first_term, second_term):
     return first_term + second_term
 
@@ -12,3 +13,4 @@ def subtract(first_term, second_term):
 
 def multiply(first_term, second_term):
     return first_term * second_term
+    
